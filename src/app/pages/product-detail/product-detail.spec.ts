865e71c3c53d3c514a0d789bf0fe97d092352fb1
Sheng Qi <sandyteksq@gmail.com>
2026-08-06 11:03:29 +0800

    fixed unit test failures (3) by setting up router providers

--- src/app/pages/product-detail/product-detail.spec.ts
+++ src/app/pages/product-detail/product-detail.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { ProductDetail } from './product-detail';
 
@@ -9,6 +10,7 @@ describe('ProductDetail', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductDetail],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductDetail);
