3861b038bd1925e249a476c89ba03e79586af57b
Htet Nandar <htetnandar.282198@gmail.com>
2026-08-12 15:41:34 +0800

    Feature: Update HTML titles and icons, enhance accessibility in image search dialog, and add admin theme styles

--- src/app/shared/components/nav-bar/nav-bar.ts
+++ src/app/shared/components/nav-bar/nav-bar.ts
@@ -1,10 +1,9 @@
 // Author: Htet Nandar (Grace)
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {RouterLinkActive, Router, RouterLink} from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CartService } from '../../../services/cart';
-import { signal } from '@angular/core'; //Junior
 import { ProductService } from '../../../services/product'; //Junior
 
 /**
@@ -15,7 +14,7 @@ import { ProductService } from '../../../services/product'; //Junior
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
